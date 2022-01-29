a4898f279af724f10dd60dbc25bcc273b7c40173
EsotericZ <cjsand03@gmail.com>
2022-01-29 15:34:07 -0700

    final update

--- README.md
+++ README.md
@@ -1,5 +1,30 @@
-# e-commerce-orm
-E-Commerce Back End built with ORM
+# ORM E-Commerce Back End
 
-#cont
-worked with Kirtley
+## Description 
+Using existing code, created the back end object-relational mapping to make Sequelize interact with a MySql database.
+
+## Table of Contents
+- [Installation](#Installation)
+- [Usage](#Usage)
+- [Contributing](#Contributing)
+- [Tests](#Tests)
+- [Questions](#Questions)
+
+## Installation
+Inquirer, mysql2 and console.table are required to run the app
+      
+## Usage
+Creating a database that can add, delete and update products, tags and categories for a company.  
+  
+Video Example  
+[![Watch](https://img.freepik.com/free-vector/video-player-interface-play-button-play-video_177006-355.jpg?size=626&ext=jpg)](https://watch.screencastify.com/v/NBfxKOk0lWjbZ3x2wHpV)
+      
+## Contributing 
+Kirtley Adams contributed to this app
+
+## Tests
+n/a
+
+## Questions
+GitHub Profile: [EsotericZ](https://www.github.com/EsotericZ)  
+Additional questions? [Contact Me](mailto:cjsand03@gmail.com)
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -61,26 +61,26 @@ router.post('/', async (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-  Product.create(req.body)
-    .then((product) => {
-      // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tagIds.length) {
-        const productTagIdArr = req.body.tagIds.map((tag_id) => {
-          return {
-            product_id: product.id,
-            tag_id,
-          };
-        });
-        return ProductTag.bulkCreate(productTagIdArr);
-      }
-      // if no product tags, just respond
-      res.status(200).json(product);
-    })
-    .then((productTagIds) => res.status(200).json(productTagIds))
-    .catch((err) => {
-      console.log(err);
-      res.status(400).json(err);
-    });
+  // Product.create(req.body)
+  //   .then((product) => {
+  //     // if there's product tags, we need to create pairings to bulk create in the ProductTag model
+  //     if (req.body.tagIds.length) {
+  //       const productTagIdArr = req.body.tagIds.map((tag_id) => {
+  //         return {
+  //           product_id: product.id,
+  //           tag_id,
+  //         };
+  //       });
+  //       return ProductTag.bulkCreate(productTagIdArr);
+  //     }
+  //     // if no product tags, just respond
+  //     res.status(200).json(product);
+  //   })
+  //   .then((productTagIds) => res.status(200).json(productTagIds))
+  //   .catch((err) => {
+  //     console.log(err);
+  //     res.status(400).json(err);
+  //   });
 });
 
 // update product
@@ -120,14 +120,13 @@ router.put('/:id', (req, res) => {
     })
     .then((updatedProductTags) => res.json(updatedProductTags))
     .catch((err) => {
-      // console.log(err);
       res.status(400).json(err);
     });
 });
 
 router.delete('/:id', async (req, res) => {
   try {
-    const deleteProduct = await Product.findById(req.params.id);
+    const deleteProduct = await Product.findByPk(req.params.id);
     await Product.destroy({
       where: {
         id: req.params.id,
